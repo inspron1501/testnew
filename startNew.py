d7a525e17ae808c702bd9fa9413ba4ebbd0594ce
inspron1501 <v.filimonova@redmadrobot.com>
2016-11-22 17:25:48 +0400

    добавлены запрс-ответ

--- startNew.py
+++ startNew.py
@@ -6,3 +6,6 @@ my_file = open("test_python_qa.txt", "w")
 logging.basicConfig(filename='log_test_python_qa.txt', level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('This is a log message.')
+
+requestsArgs = {"test": "3", "test2": "two"}
+resOne = requests.get('http://httpbin.org/get', requestsArgs)
