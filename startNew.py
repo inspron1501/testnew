400fce9b66570f36b73d5313d2f7bb97baab56ac
inspron1501 <v.filimonova@redmadrobot.com>
2016-11-22 17:42:20 +0400

    добавлена запись в txt файл

--- startNew.py
+++ startNew.py
@@ -13,5 +13,9 @@ parsed_string = json.loads(resOne.text)
 responseArgs = (parsed_string["args"])
 if requestsArgs == responseArgs:
     print('allright! Arguments correspond to the values in the "args"')
+    my_file.write(
+        'Check for equality between query parameters to the parameters in the response body - allright! Arguments correspond to the values in the "args" \n')
 else:
     print('something goes wrong =( Arguments do NOT match the values in the "arguments"')
+    my_file.write(
+        'Check for equality between query parameters to the parameters in the response body - arguments do NOT match the values in the "arguments" \n')
